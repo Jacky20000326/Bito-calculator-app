7891cd30a2f449ec4c15d27f62c65f9a4c997a45
jacky_hong <Jj0932175456@gmail.com>
2024-04-02 21:37:47 +0800

    fixed bug 0402

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    // const queryClient = new QueryClient(); // create queryClient instance
     return (
         <html lang="en">
             <body className={inter.className}>
--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,34 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import styled from "./RateTable.module.sass";
-import Image from "next/image";
-import { fetchCurrencyData } from "../apis/currencyApi";
-import { useQuery } from "@tanstack/react-query";
-import { useCurrencyStore } from "../store/currencyContextStore";
-
-import { usePathname, useRouter } from "next/navigation";
-import { PageRouteDefines } from "../defines/pageDefines";
+import { useRouter } from "next/navigation";
+import { PageRouteDefines } from "../Defines/pageDefines";
 import CurrencyItem from "@/components/currency/CurrencyItem/CurrencyItem";
 import HydratedPosts from "@/libs/react-query-lib/HydrateProvider";
 const Home = () => {
-
     const Route = useRouter();
 
     const movePageHandler = () => {
         Route.push(PageRouteDefines.rateConversion);
     };
-    
-        return (
-            <div className={styled.rateTableContainer}>
-                <div className={styled.rateTableTitle}>
-                    <div className={styled.currencyTitle}>Currency</div>
-                    <div className={styled.priceTitle}>price</div>
-                </div>
-    
-                    <div className={styled.rateColumnContainer}>
-                    <HydratedPosts>
-                        <CurrencyItem />
-                    </HydratedPosts>
-                    </div>
-                    <div
-                        className={styled.rateConversionBtn}
-                        onClick={movePageHandler}
-                    >
-                        Rate ConversionBtn
-                    </div>
-            </div>
-        );
-    }
 
+    return (
+        <div className={styled.rateTableContainer}>
+            <div className={styled.rateTableTitle}>
+                <div className={styled.currencyTitle}>Currency</div>
+                <div className={styled.priceTitle}>price</div>
+            </div>
+
+            <div className={styled.rateColumnContainer}>
+                <HydratedPosts>
+                    <CurrencyItem />
+                </HydratedPosts>
+            </div>
+            <div className={styled.rateConversionBtn} onClick={movePageHandler}>
+                Rate ConversionBtn
+            </div>
+        </div>
+    );
+};
 
 export default Home;
